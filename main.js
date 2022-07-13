708be5505131db25213e7e82aaf05a7982a981d2
SAHRIAR-ANIK <106604325+SAHRIAR-ANIK@users.noreply.github.com>
2022-07-13 14:48:02 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+new VenoBox();
+
+
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:2
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:2
+        }
+    }
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var mixer = mixitup('.nine-df');
+
+
+
+
+
+
